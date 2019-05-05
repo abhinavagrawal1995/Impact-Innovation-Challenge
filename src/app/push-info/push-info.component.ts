5e6b7f70a9d6b4d5d689323f908af03df8197c79
Abhinav Agrawal <abhinavagrawal1995@gmail.com>
2019-05-04 17:59:23 -0700

    animations

--- src/app/push-info/push-info.component.ts
+++ src/app/push-info/push-info.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./push-info.component.css']
 })
 export class PushInfoComponent implements OnInit {
-
+  done = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  success() {
+    this.done = true;
+  }
+
+  back() {
+    this.done = false;
   }
 
 }
